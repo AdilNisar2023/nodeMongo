ee1fa286c68c88afcd958e2340c15a0fea6d94a2
AdilNisar2023 <adhilnisar66@gmail.com>
2023-11-01 12:01:02 +0530

    first commit

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const connectDB = require("./DB/connect");
 const express =require("express")
 const taskroute=require('./routes/taskroute')
 const notfound=require('./Middleware/notfound')
+const errorHandlerMiddleware=require("./Middleware/errorHandler")
 const app=express()
 app.use(express.json())
 app.use('/api/v1/task',taskroute)
 app.use(notfound)
+app.use(errorHandlerMiddleware);
 const start=async ()=>{
     try{
         await connectDB(process.env.MONGO_URL);
--- controller/control.js
+++ controller/control.js
@@ -1,27 +1,24 @@
+const asyncWrapper = require("../Middleware/async");
 const Task=require("../Models/task")
-const getAllData=async (req,res)=>{
-   
-        try{
+
+
+
+const getAllData=asyncWrapper(async (req,res)=>{
             const task=await Task.find({});
 res.status(200).json({task});
-}catch(error){
-    res.status(500).json({msg:error});
-}
-}
-const createData=async (req,res)=>{
-    try{
+})
+const createData=asyncWrapper(async (req,res)=>{
+   
         const task=await Task.create(req.body);
 res.status(201).json({task});
     }
-    catch(error){
-        res.status(500).json({msg:error}); 
-       }
-    };
+    
+    );
 
     
 
-const getData=async (req,res)=>{
-    try{
+const getData=asyncWrapper(async(req,res)=>{
+    
         const {id:taskID}=req.params;
         const task=await Task.findOne({_id:taskID});
         if (!task){
@@ -29,16 +26,12 @@ const getData=async (req,res)=>{
         }
         res.status(200).json({task});
         
-            }catch (error)
-            {
-                res.status(500).json({msg:error});
-        
             }
         
-        };
+        );
 
-const updateData=async(req,res)=>{
-    try{
+const updateData=asyncWrapper(async(req,res)=>{
+    
         const {id:taskID}=req.params;
         const task=await Task.findOneAndUpdate({_id:taskID},req.body,{
             new: true,
@@ -53,15 +46,11 @@ const updateData=async(req,res)=>{
         }
        
         
-            }catch (error)
-            {
-                res.status(500).json({msg:error});
-        
             }
         
-        };
-const deleteData=async(req,res)=>{
-    try{
+        );
+const deleteData=asyncWrapper(async(req,res)=>{
+    
 const {id:taskID}=req.params;
 const task=await Task.findOneAndDelete({_id:taskID});
 if (!task){
@@ -69,11 +58,6 @@ if (!task){
 }
 res.status(200).json({task});
 
-    }catch (error)
-    {
-        res.status(500).json({msg:error});
-
     }
-
-};
+);
 module.exports={getAllData,getData,createData,updateData,deleteData}
